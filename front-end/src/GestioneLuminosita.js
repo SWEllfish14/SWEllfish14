6b5e28c5c821d677734f54f7d5e1f1342d36b2bb
Francesco Naletto <46630982+Hisukurifu24@users.noreply.github.com>
2023-07-03 15:14:41 +0200

    Rename front-end folder

---
+++ front-end/src/GestioneLuminosita.js
@@ -0,0 +1,78 @@
+import axios from "axios";
+import { useState } from 'react';
+import { useQuery } from "react-query";
+import Select from "react-select";
+import { AggiungiArea } from "./AggiungiArea";
+export function GestioneGuasti() {
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    const [areaList, setAreaList] = useState([]);
+    const [areaDaAggiungere, setAreaDaAggiungere] = useState("");
+    const [guastoList, setGuastoList] = useState([]);
+    const { } = useQuery(["amministratore"], () =>
+        axios.get("http://localhost:3002/aree")
+            .then((res) => setAreaList(res.data))
+    )
+    const { isLoading: guastiLoading } = useQuery(["guasto"], () =>
+        axios.get("http://localhost:3002/guasti")
+            .then((res) => setGuastoList(res.data))
+    )
+    const rimuoviAreaDaGuasto = (id) => {
+        axios.post("http://127.0.0.1:3002/rimuoviGuasto" , null, {
+            params: {
+              id: id
+            }}).then((res) =>
+            axios.get("http://localhost:3002/guasti")
+                .then((res) => setGuastoList(res.data)))
+    }
+
+    const aggiungiAreaGuasto = (id) => {
+        axios.post("http://127.0.0.1:3002/AggiungiGuasto" , null, {
+            params: {
+              id: id
+            }}).then((res) =>
+            axios.get("http://localhost:3002/guasti")
+                .then((res) => setGuastoList(res.data)))
+    }
+    return (
+        <>
+        <div class ="box">
+            Aggiungi una nuova area alla sezione guasti
+            <Select
+                name="amministratori"
+                options={areaList}
+                value={areaDaAggiungere}
+                onChange={setAreaDaAggiungere}
+                getOptionLabel={(area) => area.zona_geografica}
+                getOptionValue={(area) => area.ID}></Select>
+            <button class = "button is-success" onClick={() => aggiungiAreaGuasto(areaDaAggiungere)}>Aggiungi</button>
+            </div>
+            
+        <div class ="box">
+            Lista aree con guasti
+        </div>
+            {
+                guastiLoading ? <p>Loading...</p> :
+                    <ul>
+                        {guastoList.map(guasto => (
+                           <div class="tile is-ancestor">
+                            <div class="tile is-parent">
+                              <article class="tile is-child box">
+                            <li key={guasto.ID}>
+                                <h1>{guasto.zona_geografica}</h1>
+                                Guasto nell'area {guasto.id_area_illuminata}
+                                <li><button class ="button is-success" onClick={() => rimuoviAreaDaGuasto(guasto.ID)}>Rimuovi</button></li>
+                            </li>
+                            </article>
+                            </div>
+                            </div> 
+                        ))}
+                    </ul>
+                    
+            }
+            
+        </>
+
+    )
+}
