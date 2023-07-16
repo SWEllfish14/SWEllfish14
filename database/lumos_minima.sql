2f3c22928a40a91ea36c06474afefd0c04390119
Andrea <93330631+WatEagle001@users.noreply.github.com>
2023-07-16 16:29:56 +0200

    Aggiunti nuovi lampioni, sensori e gestione guasti

--- database/lumos_minima.sql
+++ database/lumos_minima.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Versione server:              11.0.2-MariaDB - mariadb.org binary distribution
 -- S.O. server:                  Win64
--- HeidiSQL Versione:            12.5.0.6677
+-- HeidiSQL Versione:            12.3.0.6589
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -51,8 +51,8 @@ CREATE TABLE IF NOT EXISTS `area_illuminata` (
 
 -- Dump dei dati della tabella lumosminima.area_illuminata: ~13 rows (circa)
 INSERT INTO `area_illuminata` (`ID`, `zona_geografica`, `stato`, `luminosita_impostata`, `luminosita_default`, `user_amministratore`) VALUES
-	(1, 'Padova_via Scrovegni', '1', 5, 2, 'admin'),
-	(2, 'Mantova', '0', 9, 1, 'andrea'),
+	(1, 'Padova', '1', 5, 2, 'admin'),
+	(2, 'Mantova', '0', 7, 1, 'andrea'),
 	(3, 'Parma', '1', 3, 0, 'jude'),
 	(4, 'Viareggio', '0', 5, 9, 'elena'),
 	(5, 'Catania', '1', 5, 9, 'claudio'),
@@ -75,7 +75,13 @@ CREATE TABLE IF NOT EXISTS `gestione_guasto` (
   CONSTRAINT `fk_username_manutentore` FOREIGN KEY (`Username_manutentore`) REFERENCES `manutentore` (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella lumosminima.gestione_guasto: ~0 rows (circa)
+-- Dump dei dati della tabella lumosminima.gestione_guasto: ~5 rows (circa)
+INSERT INTO `gestione_guasto` (`Username_manutentore`, `id_guasto`) VALUES
+	('manutentore', 1),
+	('manutentore', 2),
+	('manutantore2', 3),
+	('manutantore3', 4),
+	('manutantore4', 5);
 
 -- Dump della struttura di tabella lumosminima.guasto
 CREATE TABLE IF NOT EXISTS `guasto` (
@@ -87,10 +93,13 @@ CREATE TABLE IF NOT EXISTS `guasto` (
   CONSTRAINT `fk_id_area_illuminata_guasto` FOREIGN KEY (`id_area_illuminata`) REFERENCES `area_illuminata` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella lumosminima.guasto: ~6 rows (circa)
+-- Dump dei dati della tabella lumosminima.guasto: ~5 rows (circa)
 INSERT INTO `guasto` (`ID`, `zona_geografica`, `id_area_illuminata`) VALUES
 	(1, 'Gorizia', 6),
-	(2, 'Padova', 1);
+	(2, 'Padova', 1),
+	(3, 'Mantova', 2),
+	(4, 'Viareggio', 4),
+	(5, 'Napoli', 9);
 
 -- Dump della struttura di tabella lumosminima.lampione
 CREATE TABLE IF NOT EXISTS `lampione` (
@@ -102,25 +111,40 @@ CREATE TABLE IF NOT EXISTS `lampione` (
   `luminosita_default` int(11) NOT NULL,
   `luminosita_impostata` int(11) NOT NULL,
   `id_area_illuminata` int(11) NOT NULL,
+  PRIMARY KEY (`IP`),
+  UNIQUE KEY `ID` (`ID`),
   KEY `fk_id_area_illuminata_lampioni` (`id_area_illuminata`),
   CONSTRAINT `fk_id_area_illuminata_lampioni` FOREIGN KEY (`id_area_illuminata`) REFERENCES `area_illuminata` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella lumosminima.lampione: ~13 rows (circa)
+-- Dump dei dati della tabella lumosminima.lampione: ~26 rows (circa)
 INSERT INTO `lampione` (`IP`, `ID`, `polling_time`, `status`, `iterazione`, `luminosita_default`, `luminosita_impostata`, `id_area_illuminata`) VALUES
-	('2003001255', '123', 3, '1', 'push', 5, 8, 5),
 	('192.168.2.25', '124', 3, '1', 'push', 5, 8, 1),
-	('2003001250', '125', 3, '1', 'push', 2, 10, 1),
-	('2003001230', '126', 3, '1', 'push', 2, 10, 2),
-	('2003001229', '127', 3, '1', 'push', 6, 10, 2),
 	('2003001228', '128', 3, '1', 'push', 0, 10, 3),
-	('2003001238', '129', 3, '1', 'push', 3, 7, 4),
-	('2013001238', '130', 3, '1', 'push', 2, 7, 4),
+	('2003001229', '127', 3, '0', 'push', 6, 7, 2),
+	('2003001230', '126', 3, '0', 'push', 6, 7, 2),
+	('2003001238', '129', 3, '0', 'push', 3, 5, 4),
+	('2003001250', '125', 3, '0', 'push', 2, 10, 1),
+	('2003001255', '123', 3, '0', 'push', 5, 8, 5),
+	('2003001256', '136', 3, '0', 'push', 5, 8, 8),
+	('2003001257', '137', 3, '0', 'push', 5, 8, 8),
+	('2003001258', '138', 3, '0', 'push', 5, 8, 9),
+	('2003001259', '139', 3, '0', 'push', 5, 8, 9),
+	('2003001260', '140', 3, '0', 'push', 5, 8, 10),
+	('2003001261', '141', 3, '0', 'push', 5, 8, 10),
+	('2003001262', '142', 3, '0', 'push', 5, 8, 11),
+	('2003001263', '143', 3, '0', 'push', 5, 8, 11),
+	('2003001264', '144', 3, '0', 'push', 5, 8, 12),
+	('2003001265', '145', 3, '0', 'push', 5, 8, 12),
+	('2003001266', '146', 3, '0', 'push', 5, 8, 12),
+	('2003001267', '147', 3, '0', 'push', 5, 8, 13),
+	('2003001268', '148', 3, '0', 'push', 5, 8, 13),
 	('2013001221', '131', 3, '1', 'push', 2, 7, 5),
-	('2013001233', '132', 3, '1', 'push', 5, 7, 5),
+	('2013001233', '132', 3, '0', 'push', 5, 7, 5),
+	('2013001238', '130', 3, '1', 'push', 2, 5, 4),
 	('2014001233', '133', 3, '1', 'push', 7, 7, 6),
 	('2014001288', '134', 3, '1', 'push', 7, 9, 7),
-	('2015001288', '135', 3, '1', 'push', 8, 9, 7);
+	('2015001288', '135', 3, '0', 'push', 8, 9, 7);
 
 -- Dump della struttura di tabella lumosminima.manutentore
 CREATE TABLE IF NOT EXISTS `manutentore` (
@@ -129,8 +153,11 @@ CREATE TABLE IF NOT EXISTS `manutentore` (
   PRIMARY KEY (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella lumosminima.manutentore: ~0 rows (circa)
+-- Dump dei dati della tabella lumosminima.manutentore: ~4 rows (circa)
 INSERT INTO `manutentore` (`Username`, `password`) VALUES
+	('manutantore2', 'abcd'),
+	('manutantore3', 'abcd'),
+	('manutantore4', 'abcd'),
 	('manutentore', 'abcd');
 
 -- Dump della struttura di tabella lumosminima.sensore
@@ -142,20 +169,27 @@ CREATE TABLE IF NOT EXISTS `sensore` (
   `polling_time` int(11) NOT NULL,
   `id_area_illuminata` int(11) NOT NULL,
   PRIMARY KEY (`IP`),
+  UNIQUE KEY `IP` (`IP`),
   KEY `fk_id_area_illuminata` (`id_area_illuminata`),
   CONSTRAINT `fk_id_area_illuminata` FOREIGN KEY (`id_area_illuminata`) REFERENCES `area_illuminata` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella lumosminima.sensore: ~8 rows (circa)
+-- Dump dei dati della tabella lumosminima.sensore: ~14 rows (circa)
 INSERT INTO `sensore` (`IP`, `zona_geografica`, `iterazione`, `raggio_azione`, `polling_time`, `id_area_illuminata`) VALUES
 	('122288798', 'cittadella mura', 'pull', 25, 3, 7),
 	('123188762', 'Parma', 'pull', 25, 3, 3),
-	('123188798', 'Viareggio varo Brombeis', 'pull', 25, 3, 4),
+	('123188798', 'Viareggio varco Brombeis', 'pull', 25, 3, 4),
 	('123198762', 'Mantova', 'pull', 25, 3, 2),
 	('192168116', 'Gorizia', 'pull', 10, 3, 6),
 	('192168123', 'Padova', 'pull', 10, 3, 1),
 	('198188798', 'Catania Parco', 'pull', 25, 3, 5),
-	('198288798', 'gorizia', 'pull', 25, 3, 6);
+	('198288798', 'gorizia', 'pull', 25, 3, 6),
+	('198288799', 'Roma', 'pull', 25, 3, 8),
+	('198288800', 'Roma', 'pull', 25, 3, 8),
+	('198288801', 'Napoli', 'pull', 25, 3, 9),
+	('198288802', 'Verona porta nuova', 'pull', 25, 3, 10),
+	('198288803', 'Torino', 'pull', 25, 3, 11),
+	('198288804', 'Torino', 'pull', 25, 3, 12);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
