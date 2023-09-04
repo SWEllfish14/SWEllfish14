38ce5acca2cb63771b1371d827566c8bf10c4a25
Giah97 <77204350+Giah97@users.noreply.github.com>
2023-09-04 10:53:41 +0200

    riaggiunta divisione lista guasti aperti e chiusi

--- LumosMinima/client/src/view/ListaGuastiView.tsx
+++ LumosMinima/client/src/view/ListaGuastiView.tsx
@@ -10,7 +10,71 @@ import { Link } from "react-router-dom";
 <div id="guasti" className ="tile-is-ancestor">
 <div className="tile-is-parent">
 <article className="art">
-<h1 className="title">Lista Guasti a Sistema</h1>
+
+<h1>Lista Guasti a Sistema</h1>
+
+<div className="columns">
+    
+    <div className="menu-label column is-half">
+        <h2>Guasti aperti</h2>
+        {viewModel.isLoading() ? <p>Loading...</p> :
+        <ul>
+            {viewModel.guastiAperti()?.map(guasto => (
+                    <div className ="tile is-ancestor">
+                    <div className="tile is-parent is-small">
+                    <article className="tile is-child box">
+                    
+                <li key={guasto.ID}>
+                    
+                    {guasto.data_rilevamento.toString()} 
+                    <p>ID: {guasto.ID}: Guasto a {guasto["area.città"]}, zona {guasto["area.zona_geografica_città"]}
+                    </p>
+                    <Link to={{pathname: `/guasti/${guasto.ID}`}}><button className="button is-success">Vai ai dettagli</button></Link>
+                
+                </li>
+                </article>
+                </div>
+                </div>
+            ))}
+            
+        </ul>
+        }
+    </div>
+
+    <div className=" menu-label column is-half">
+    <h2>Guasti chiusi</h2>
+    {
+        <ul>
+            {viewModel.guastiChiusi()?.map(guasto => (
+                    <div className ="tile is-ancestor">
+                    <div className="tile is-parent is-small">
+                    <article className="tile is-child box">
+                    
+                <li key={guasto.ID}>
+                    
+                    {guasto.data_rilevamento.toString()} 
+                    <p>ID: {guasto.ID}: Guasto a {guasto["area.città"]}, zona {guasto["area.zona_geografica_città"]}
+                    </p>
+                    <Link to={{pathname: `/guasti/${guasto.ID}`}}><button className="button is-success">Vai ai dettagli</button></Link>
+                
+                </li>
+                </article>
+                </div>
+                </div>
+            ))}
+            
+        </ul>
+        }
+    </div>
+</div>
+            
+</article>
+</div>
+</div>
+</div>
+)
+
+{/* <h1 className="title">Lista Guasti a Sistema</h1>
 <p className="menu-label">
     
     {viewModel.isLoading() ? <p>Loading...</p> :
@@ -40,9 +104,9 @@ import { Link } from "react-router-dom";
 </article>
 </div>
 </div>
-</div>
+</div> */}
 
-   )
+   
  
 
 
