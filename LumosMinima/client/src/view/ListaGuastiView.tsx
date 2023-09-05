78f520bd7c8a56bf8ff9ec9bb2c2b3b8d008f4d2
Giah97 <77204350+Giah97@users.noreply.github.com>
2023-09-05 09:48:46 +0200

    allineate colonne lista guasti

--- LumosMinima/client/src/view/ListaGuastiView.tsx
+++ LumosMinima/client/src/view/ListaGuastiView.tsx
@@ -6,70 +6,63 @@ import { Link } from "react-router-dom";
    viewModel:IListaGuastiViewModel;
  }
  const ListaGuastiView = ({viewModel}: Props) => (
-  <div>
-<div className ="tile is-ancestor">
-<div className="tile is-parent">
-<article className="tile is-child box">
-<h1>Lista Guasti a Sistema</h1>
 
-<div className="columns">
-    
-    <div className="menu-label column is-half">
-        <h2>Guasti aperti</h2>
-        {viewModel.isLoading() ? <p>Loading...</p> :
-        <ul>
-            {viewModel.guastiAperti()?.map(guasto => (
-                    <div className ="tile is-ancestor">
-                    <div className="tile is-parent is-small">
-                    <article className="tile is-child box">
+<div>
+    <div className ="box">
+        <h1 className="title is-4">Lista Guasti a Sistema</h1>
+            <div className="columns">
+            <div className="column is-half">
+            <h2>Guasti aperti</h2>
+            {viewModel.isLoading() ? <p>Loading...</p> :
+            <ul>
+                {viewModel.guastiAperti()?.map(guasto => (
+                        <div className ="tile is-ancestor">
+                        <div className="tile is-parent is-small">
+                        <article className="tile is-child box">
+                        
+                    <li key={guasto.ID}>
+                        
+                        {guasto.data_rilevamento.toString()} 
+                        <p>ID: {guasto.ID}: Guasto a {guasto["area.città"]}, zona {guasto["area.zona_geografica_città"]}
+                        </p>
+                        <Link to={{pathname: `/guasti/${guasto.ID}`}}><button className="button is-success">Vai ai dettagli</button></Link>
                     
-                <li key={guasto.ID}>
-                    
-                    {guasto.data_rilevamento.toString()} 
-                    <p>ID: {guasto.ID}: Guasto a {guasto["area.città"]}, zona {guasto["area.zona_geografica_città"]}
-                    </p>
-                    <Link to={{pathname: `/guasti/${guasto.ID}`}}><button className="button is-success">Vai ai dettagli</button></Link>
+                    </li>
+                    </article>
+                    </div>
+                    </div>
+                ))}
                 
-                </li>
-                </article>
-                </div>
-                </div>
-            ))}
-            
-        </ul>
-        }
-    </div>
-
-    <div className=" menu-label column is-half">
-    <h2>Guasti chiusi</h2>
-    {
-        <ul>
-            {viewModel.guastiChiusi()?.map(guasto => (
-                    <div className ="tile is-ancestor">
-                    <div className="tile is-parent is-small">
-                    <article className="tile is-child box">
+            </ul>
+            }
+            </div>
+            <div className="column is-half">
+            <h2>Guasti chiusi</h2>
+        {
+            <ul>
+                {viewModel.guastiChiusi()?.map(guasto => (
+                        <div className ="tile is-ancestor">
+                        <div className="tile is-parent is-small">
+                        <article className="tile is-child box">
+                        
+                    <li key={guasto.ID}>
+                        
+                        {guasto.data_rilevamento.toString()} 
+                        <p>ID: {guasto.ID}: Guasto a {guasto["area.città"]}, zona {guasto["area.zona_geografica_città"]}
+                        </p>
+                        <Link to={{pathname: `/guasti/${guasto.ID}`}}><button className="button is-success">Vai ai dettagli</button></Link>
                     
-                <li key={guasto.ID}>
-                    
-                    {guasto.data_rilevamento.toString()} 
-                    <p>ID: {guasto.ID}: Guasto a {guasto["area.città"]}, zona {guasto["area.zona_geografica_città"]}
-                    </p>
-                    <Link to={{pathname: `/guasti/${guasto.ID}`}}><button className="button is-success">Vai ai dettagli</button></Link>
+                    </li>
+                    </article>
+                    </div>
+                    </div>
+                ))}
                 
-                </li>
-                </article>
-                </div>
-                </div>
-            ))}
-            
-        </ul>
-        }
+            </ul>
+            }
+            </div>
+        </div>
     </div>
-</div>
-            
-</article>
-</div>
-</div>
 </div>
 
    )
